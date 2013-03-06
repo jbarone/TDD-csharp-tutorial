5ca6d979a84d033fceb1376c40c3f912b730cf95
Joshua Barone <jbarone@justbecausesoftware.com>
2013-03-05 20:52:08 -0600

    Hello Step #3

---
+++ tdd-csharp-tutorial.Tests/Unit/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using tdd_csharp_tutorial.Controllers;
+
+namespace tdd_csharp_tutorial.Tests.Unit.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            HomeController testSubject = new HomeController();
+            ViewResult result = testSubject.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ tdd-csharp-tutorial/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tdd_csharp_tutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return null;
+        }
+    }
+}
